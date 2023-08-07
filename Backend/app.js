1258d22f5adee9c146a0acde5fabfbb562a982b4
upendra672 <upendaryadav672@gmail.com>
2023-08-07 17:02:21 +0530

    order module completed also backend almost completed

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,23 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-const errorMiddleware = require("./middleware/error") 
-
+const errorMiddleware = require("./middleware/error");
 
 // Middleware to parse incoming JSON data
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 //routes import
-const product = require('./routes/productRoute');
-const user = require('./routes/userRoute');
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
-app.use("/api/v1",product)
-app.use("/api/v1",user)
-
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //middleware for error
-app.use(errorMiddleware)
-
-
+app.use(errorMiddleware);
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -3,34 +3,28 @@ const dotenv = require("dotenv");
 const ConnectDatabase = require("./config/database");
 
 //Handling Uncaught errors
-process.on("UncaughtException",(err)=>{
+process.on("UncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting the server due to Handling Uncaught errors`);
   process.exit(1);
-
-})
-
+});
 
 //config
-dotenv.config({path: "Backend/config/config.env",});
+dotenv.config({ path: "Backend/config/config.env" });
 
 //connect Database
 ConnectDatabase();
-
 
 //establishing server
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
-
-
 //Unhandled Promise Rejection
-
-process.on("unhandledRejection", err=>{
+process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting the server due to Unhandled Promise Rejection`);
-  server.close(()=>{
+  server.close(() => {
     process.exit(1);
   });
-})
+});
