49c3236b28438b5086cfda49a3341f27a35e0ad6
upendra672 <upendaryadav672@gmail.com>
2023-08-09 23:06:58 +0530

    normal updates

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,22 +12,7 @@ import {
 const reducer = combineReducers({
   products: productsReducer,
   productDetails: productDetailsReducer,
-  // user: userReducer,
-  // profile: profileReducer,
-  // forgotPassword: forgotPasswordReducer,
-  // cart: cartReducer,
-  // newOrder: newOrderReducer,
-  // myOrders: myOrdersReducer,
-  // orderDetails: orderDetailsReducer,
   newReview: newReviewReducer,
-  // newProduct: newProductReducer,
-  // product: productReducer,
-  // allOrders: allOrdersReducer,
-  // order: orderReducer,
-  // allUsers: allUsersReducer,
-  // userDetails: userDetailsReducer,
-  // productReviews: productReviewsReducer,
-  // review: reviewReducer,
 });
 
 let initialState = {
